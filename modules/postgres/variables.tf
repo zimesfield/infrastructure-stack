e4a1c946c6057f9ec45d8aaaa21bf9033f2b5858
cyprian.omenuko <cyprian.omenuko@unite.eu>
2023-12-15 14:57:16 +0100

    Added default postgres version

--- modules/postgres/variables.tf
+++ modules/postgres/variables.tf
@@ -9,10 +9,12 @@ variable "app_name" {
 variable "app_version" {
   description = "The version of image"
   type        = string
+  default     = "13.1.2"
 }
 variable "chart_name" {
   description = "The name of the Kubernetes chart"
   type        = string
+  default     = "postgres"
 }
 variable "db_password" {
   description = "The password of the database"

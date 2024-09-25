81ac0cd66a1cc1328536de57ca58ef82b7ba47b4
cyprian.omenuko <cyprian.omenuko@unite.eu>
2024-09-25 19:18:28 +0100

    Removed unused infrastructure files

--- modules/postgres/outputs.tf
+++ modules/postgres/outputs.tf
@@ -1,17 +1,17 @@
 output "database_host" {
   description = "The hostname of the PostgreSQL database server"
-  value       = "${helm_release.postgresql_helm_release.id}-postgresql"
+  value       = "${helm_release.postgresql.id}-postgresql"
 }
 
 output "database_secret" {
   description = "The password of the PostgreSQL database server"
-  value       = helm_release.postgresql_helm_release.metadata
+  value       = helm_release.postgresql.metadata
 }
 
 output "database_host_version" {
-  value = helm_release.postgresql_helm_release.version
+  value = helm_release.postgresql.version
 }
 
 output "database_host_status" {
-  value = helm_release.postgresql_helm_release.status
+  value = helm_release.postgresql.status
 }

36362c64dde5e80c7db0bdb4661dd4e9232ba8db
cyprian.omenuko <cyprian.omenuko@unite.eu>
2024-09-25 10:57:24 +0100

    Added backend terraform state

---
+++ modules/backend/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "2.28.0"
+    }
+  }
+}
+
+provider "linode" {
+  token = var.token
+}
+
+data "linode_object_storage_cluster" "primary" {
+  id = var.region
+}
+
+resource "linode_object_storage_key" "storage_key" {
+  label = var.storage_key
+}
+
+resource "linode_object_storage_bucket" "terraform_state" {
+  cluster    = data.linode_object_storage_cluster.primary.id
+  label      = var.label
+  access_key = linode_object_storage_key.storage_key.access_key
+  secret_key = linode_object_storage_key.storage_key.secret_key
+}
+
+resource "linode_object_storage_object" "object_storage" {
+  bucket  = linode_object_storage_bucket.terraform_state.label
+  cluster = data.linode_object_storage_cluster.primary.id
+  key     = var.object_storage_key
+
+  secret_key = linode_object_storage_key.storage_key.secret_key
+  access_key = linode_object_storage_key.storage_key.access_key
+
+  source = pathexpand("~/terraform_test.txt")
+}
+
+# resource "linode_object_storage_object" "object2" {
+#   bucket  = linode_object_storage_bucket.mybucket-j1145.label
+#   cluster = data.linode_object_storage_cluster.primary.id
+#   key     = "freetext-object"
+#
+#   secret_key = linode_object_storage_key.storagekey.secret_key
+#   access_key = linode_object_storage_key.storagekey.access_key
+#
+#   content          = "This is the content of the Object..."
+#   content_type     = "text/plain"
+#   content_language = "en"
+# }
